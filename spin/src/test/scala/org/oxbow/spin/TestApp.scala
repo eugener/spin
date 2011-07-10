c13d0b5f3c8758d35a8ddf757fb30ae5e340d4e5
Eugene Ryzhikov <eryzhikov@gmail.com>
2011-07-10 17:27:12 -0400

    initial commit

---
+++ spin/src/test/scala/org/oxbow/spin/TestApp.scala
@@ -0,0 +1,25 @@
+package org.oxbow.spin
+import com.vaadin.ui.Window
+import com.vaadin.ui.Button
+import com.vaadin.ui.Label
+import com.vaadin.ui.Button.ClickListener
+
+class TestApp extends com.vaadin.Application {
+
+    private var window: Window = null
+
+    override def init() = {
+        
+        window = new Window("Spin Test Application")
+        setMainWindow(window)
+        val button = new Button("Click Me")
+        button.addListener(new ClickListener {
+            def buttonClick( event: Button#ClickEvent ) {
+                window.addComponent(new Label("Thank you for clicking"))
+            }
+        })
+        window.addComponent(button)
+        
+    }
+    
+}
